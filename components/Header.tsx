99949547eb2444a1a19a8e8ee95935346b50f65e
Victpria Okonnah <victoriaokonnah@gmail.com>
2023-09-06 10:59:03 -0700

    blurred

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import {FaFacebook, FaHome, FaSun} from 'react-icons/fa'
 export default function Header() {
   return (
     <header className='w-full relative z-10 '>
-        <div className='h-[17rem] w-[17rem] bg-[#fee30290] absolute top-[15%] right-0 -z-10 blur-md rounded-full'></div>
+        <div className='h-[17rem] w-[17rem] bg-[#fee30290] absolute top-[15%] right-0 -z-10 blur-[7rem] rounded-full'></div>
         
         <nav className='w-[90%] fixed top-4 md:hidden'>
             <div className='w-[90%] sm:w-[25rem] mx-auto  bg-primary rounded-full py-2 px-[1rem] flex items-center justify-between'>
