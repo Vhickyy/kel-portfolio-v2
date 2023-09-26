e1a766cfcc9de8990a99fe6039d7293cebec24dc
Victpria Okonnah <victoriaokonnah@gmail.com>
2023-09-26 15:18:28 -0700

    added about content

--- components/About.tsx
+++ components/About.tsx
@@ -3,13 +3,21 @@ import React from 'react'
 function About() {
   return (
     <section className='pt-[8rem]'>
-      <div className='flex items-center gap-4'>
-        <h2 className='font-extrabold text-3xl text-primary'> 01.</h2>
-        <h2 className='font-extrabold text-3xl text-accent'>About Me</h2>
+      <div className='flex items-center justify-center'>
+        {/* <h2 className='font-extrabold text-3xl text-primary'> 01.</h2> */}
+        <h2 className='font-extrabold text-3xl text-textColor'>About <span className="text-primary">Me</span></h2>
         {/* <div className='w-[7rem] h-[1px] bg-textgray'></div> */}
       </div>
       <div>
-        <p className='text-textgray leading-loose mt-6   tracking-wider'>Lorem ipsum dolor sit amet consectetur adipisicing elit. A temporibus reprehenderit quidem cupiditate ipsam voluptatem eius dolorum nulla debitis? Asperiores, repellat veniam earum quidem ducimus soluta ullam quis commodi, voluptas fugiat quod inventore? Labore odit doloribus rerum voluptatibus vel at dolorum deserunt placeat distinctio eveniet ex, dicta, maiores cumque harum!</p>
+        <p className='text-textgray leading-loose mt-6   tracking-wider'>
+        I am a Business Management graduate, a Business Assistant and a <span className="text-primary font-bold">Canva Expert</span> with more than 3years experience. I am also a <span className="text-primary font-bold">Canvassador</span>.
+        {/* </p> */}
+        {/* <p className='text-textgray leading-loose mt-6   tracking-wider'> */}
+        I take my client's work very seriously and always deliver on time.
+        My works are always exceptional and extraordinary; reason why my clients stick with me.
+        I create attractive and scroll-stopping designs for business owners that help them grab the attention of their target/potential customers.
+        I also tutor people on how to make better use of Canva. I have trained over a thousand people.
+        </p>
       </div>
     </section>
   )
