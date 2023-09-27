7282c9d0cbf5964c69e8ff483bec5a80ef38e4d4
Victpria Okonnah <victoriaokonnah@gmail.com>
2023-09-27 15:52:43 -0700

    about content and styling added

--- components/About.tsx
+++ components/About.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image'
 import React from 'react'
+import Img from "../assets/aboutImg3.png"
 
 function About() {
   return (
@@ -6,17 +8,29 @@ function About() {
       {/* <div className='flex items-center justify-center'> */}
         {/* <h2 className='font-extrabold text-3xl text-primary'> 01.</h2> */}
         <h2 className='font-extrabold text-2xl sm:text-3xl text-textColor text-center'>About Me</h2>
-        {/* <div className='w-[7rem] h-[1px] bg-textgray'></div> */}
+        
       {/* </div> */}
-      <div>
-        <p className='text-textgray leading-loose mt-6   tracking-wider'>
-        I am a Business Management graduate, a Business Assistant and a <span className="text-primary font-bold">Canva Expert</span> with more than 3years experience. I am also a <span className="text-primary font-bold">Canvassador</span>. I take my client's work very seriously and always deliver on time.
-        </p>
-        <p className='text-textgray leading-loose mt-6   tracking-wider'>
-        My works are always exceptional and extraordinary; reason why my clients stick with me.
-        I create attractive and scroll-stopping designs for business owners that help them grab the attention of their target/potential customers.
-        I also tutor people on how to make better use of Canva. I have trained over a thousand people.
-        </p>
+      <div className='mt-6 flex gap-4 flex-col sm:flex-row-reverse sm:items-center sm:justify-between'>
+        <div className='relative w-full h-[20rem] bg-primary sm:w-[35%] rounded-lg'>
+          <Image
+          alt="About image"
+          width={350}
+          height={300}
+          src={Img}
+          // className='absolute top-0 left-1/2'
+          className=' absolute top-0 h-full object-cover left-1/2 transform -translate-x-1/2'
+          />
+        </div>
+        <div className='sm:w-[60%] '>
+          <p className='text-textgray leading-loose tracking-wider'>
+          I am a Business Management graduate, a Business Assistant and a <span className="text-primary font-bold ">Canva Expert</span> with more than 3years experience. I am also a <span className="text-primary font-bold">Canvassador</span>. I take my client's work very seriously and always deliver on time.
+          </p>
+          <p className='text-textgray leading-loose mt-6   tracking-wider'>
+          {/* My works are always exceptional and extraordinary; reason why my clients stick with me. */}
+          I create attractive and scroll-stopping designs for business owners that help them grab the attention of their target/potential customers.
+          I also tutor people on how to make better use of Canva. I have trained over a thousand people.
+          </p>
+        </div>
       </div>
     </section>
   )
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Experience() {
   return (
     <section className='pt-[8rem]'>
-      <div className='flex items-center gap-4'>
-        <h2 className='font-extrabold text-3xl text-primary'> 02.</h2>
-        <h2 className='font-extrabold text-3xl text-accent'>Experience Me</h2>
+      {/* <div className='flex items-center gap-4'> */}
+        {/* <h2 className='font-extrabold text-3xl text-primary'> 02.</h2> */}
+        <h2 className='font-extrabold text-2xl sm:text-3xl text-textColor text-center'>Experience</h2>
         {/* <div className='w-[7rem] h-[1px] bg-textgray'></div> */}
-      </div>
+      {/* </div> */}
       <div>
         <p className='text-textgray leading-loose mt-6   tracking-wider'>Lorem ipsum dolor sit amet consectetur adipisicing elit. A temporibus reprehenderit quidem cupiditate ipsam voluptatem eius dolorum nulla debitis? Asperiores, repellat veniam earum quidem ducimus soluta ullam quis commodi, voluptas fugiat quod inventore? Labore odit doloribus rerum voluptatibus vel at dolorum deserunt placeat distinctio eveniet ex, dicta, maiores cumque harum!</p>
       </div>
