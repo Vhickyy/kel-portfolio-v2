c25095e96a5d68fc077bfff0682650ccfb0978cc
VICTORIA <victoria okonnah>
2023-10-22 06:10:47 -0700

    added edit and delete icon to experience

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FaEdit, FaTrash} from "react-icons/fa"
 interface exper {
   mode?: boolean
 }
@@ -31,7 +32,7 @@ const experience = [
 ]
 function Experience({mode}:exper) {
   return (
-    <section className={mode ? 'pt-[2rem]' : 'pt-[8rem]'}>
+    <section className={mode ? 'py-[2rem]' : 'pt-[8rem]'}>
         <h2 className='font-extrabold text-2xl sm:text-3xl text-textColor text-center'>Experience</h2>
       <div className='text-textgray flex flex-col sm:flex-row flex-wrap justify-between gap-y-6 mt-12'>
         {experience.map((exp,index)=>{
@@ -42,6 +43,10 @@ function Experience({mode}:exper) {
                 <p>{exp.workplace}</p>
                 <p>{exp.role}</p>
               </div>
+              {mode && <div className="flex justify-end gap-3 pr-2 items-center text-primary">
+                <FaEdit/>
+                <FaTrash/>
+              </div>}
               <div className='absolute top-1 left-0 h-4 w-4 rounded-full bg-primary'></div>
               <div className='absolute h-full w-[1px] top-1 left-2 bg-primary'></div>
             </div>
