693998812ac4f415d34c922b8e2bdedd379f79f3
Victoria Okonnah <victoriaokonnah@gmail.com>
2023-11-21 12:52:01 -0800

    added functionality to open and close fixed wrapper

--- app/dashboard/components/Fixed.tsx
+++ app/dashboard/components/Fixed.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 
-const Fixed = ({page}:{page:string}) => {
+const Fixed = ({page,closeForm}:{page:string,closeForm: () => void}) => {
   return (
     <div className='fixed inset-0 bg-bgColorDim z-50 grid place-items-center'>
         <form className='bg-bgColor p-[1rem] w-[85%] md:w-[500px] grid gap-[1rem]'>
             <div className='flex justify-between items-center'>
                 <h1 className='font-semibold text-center text-2xl'>Add {page}</h1>
-                <FaTimes className="text-primary cursor-pointer"/>
+                <FaTimes className="text-primary cursor-pointer" onClick={closeForm}/>
             </div>
             <div>
                 <label htmlFor={`${page}:`}>{page}</label>
--- app/dashboard/skills/page.tsx
+++ app/dashboard/skills/page.tsx
@@ -1,12 +1,21 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Skills from "@/app/components/Skills"
 import Fixed from '../components/Fixed'
 
 function skills() {
+  const [open,setOpen] = useState(false);
+  const openForm = () => {
+    setOpen(true)
+  }
+  const closeForm = () => {
+    setOpen(false)
+  }
   return (
     <div className='text-white'>
-      <Fixed page="Skill" />
+      {open ? <Fixed page="Skill" closeForm={closeForm}/> : null}
+      <button className="bg-primary px-4 py-2 text-bgColor" onClick={openForm}>Add Skill</button>
       <Skills mode />
     </div>
   )
