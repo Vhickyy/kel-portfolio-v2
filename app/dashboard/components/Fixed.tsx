6713e489d9aa7363ea1a7d35aac62c98026242bc
Victoria Okonnah <victoriaokonnah@gmail.com>
2023-11-21 11:38:38 -0800

    styling for skill props

--- app/dashboard/components/Fixed.tsx
+++ app/dashboard/components/Fixed.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import { FaTimes } from 'react-icons/fa'
 
 const Fixed = ({page}:{page:string}) => {
   return (
     <div className='fixed inset-0 bg-bgColorDim z-50 grid place-items-center'>
-        <form className='bg-bgColor p-3 w-[80%] md:w-[600px]'>
-            <h1>Add {page}</h1>
+        <form className='bg-bgColor p-[1rem] w-[85%] md:w-[500px] grid gap-[1rem]'>
+            <div className='flex justify-between items-center'>
+                <h1 className='font-semibold text-center text-2xl'>Add {page}</h1>
+                <FaTimes className="text-primary cursor-pointer"/>
+            </div>
             <div>
                 <label htmlFor={`${page}:`}>{page}</label>
-                <input type="text" placeholder={`Enter ${page.toLowerCase()}`} className='rounded w-full'/>
+                <input type="text" placeholder={`Enter ${page.toLowerCase()}`} className='rounded w-full p-2 border border-bgColor'/>
             </div>
+            {page === "Skill" && <div>
+                <label htmlFor="Proficeiency:">Proficiency</label>
+                <input type="text" placeholder='Proficiency' className='rounded w-full p-2 border border-bgColor'/>
+            </div>}
             <div>
-                <label htmlFor="Proficeiency:"></label>
-                <input type="text" placeholder='Proficiency' className='rounded w-full'/>
-            </div>
-            <div>
-                <button>Create</button>
-                <button>discard</button>
+                <button className='bg-primary text-bgColor px-4 py-2 rounded'>Create</button>
+                <button className='bg-primary text-bgColor px-4 py-2 rounded'>Discard</button>
             </div>
         </form>
     </div>
