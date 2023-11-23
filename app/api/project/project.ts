473c730a95867a8e878ce13372d101636fdede7f
Victoria Okonnah <victoriaokonnah@gmail.com>
2023-11-23 06:12:25 -0800

    fixing not a module error

--- app/api/project/project.ts
+++ app/api/project/project.ts
@@ -1,13 +1,13 @@
 // import connect from "@/utils/db";
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 // connect();
 
 
-// export async function POST(request:Request){
-//     const skill:any = await request.json();
-//     console.log(skill);
-//     return NextResponse.json(skill)
-// }
+export async function POST(request:Request){
+    const skill:any = await request.json();
+    console.log(skill);
+    return NextResponse.json(skill)
+}
 
 // export async function PATCH(request:Request){
 //     const skill:any = await request.json();
